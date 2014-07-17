c081f5f2f64720b4426b3242d610fa8d158b64ef
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-07-17 13:11:59 +0530

    front page sidebar

--- base-front-page.php
+++ base-front-page.php
@@ -1,7 +1,11 @@
 <?php get_template_part('templates/layout/head', 'front-page'); ?>
   <body <?php body_class(); ?>>
 
-  <?php include roots_template_path(); ?>
+  <?php get_template_part('templates/layout/header'); ?>
+
+  <div id="wrapper">
+    <?php include roots_template_path(); ?>
+  </div>
 
   <?php get_template_part('templates/layout/footer', 'front-page'); ?>
 
