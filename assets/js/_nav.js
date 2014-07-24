b3930b7c4542677ea600eeea6dca7fcce93f0fe7
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-07-24 22:04:38 +0530

    font size, tweaks

--- assets/js/_nav.js
+++ assets/js/_nav.js
@@ -1,6 +1,7 @@
 (function($) {
   $(document).ready(function () {
-    $('#pull-nav').on('click', function () {
+    $('#pull-nav').on('click', function (e) {
+      e.preventDefault();
       $('body').toggleClass('nav-shown');
     });
 
