bcca719d1f02e0308ce14496297537ef6bcb130d
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-07-31 17:26:41 +0530

    nav change

--- templates/navigation/main.php
+++ templates/navigation/main.php
@@ -4,8 +4,8 @@
       <li><a href="/">Home</a></li>
       <li><a href="/why-ultra">Why Ultra</a></li>
       <li><a href="/registration">Registration</a></li>
-      <li><a href="/race-day-info">Race Day Info</a></li>
-      <li><a href="#">Records</a></li>
+      <li><a href="/race-day">Race Day</a></li>
+      <li><a href="#">Hall of Fame</a></li>
       <li><a href="#">Gallery</a></li>
       <li><a href="/behind-the-ultra">Behind the Ultra</a></li>
     </ul>
