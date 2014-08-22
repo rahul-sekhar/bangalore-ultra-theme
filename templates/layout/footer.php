fec229a4fa3c270f3dd944e024264443ac5d5228
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-22 10:35:09 +0530

    load typekit later, optimise text images

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,1 +1,4 @@
 <?php wp_footer(); ?>
+
+<script type="text/javascript" src="//use.typekit.net/uxx3dqf.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
--- templates/layout/head.php
+++ templates/layout/head.php
@@ -13,8 +13,5 @@
 
     <?php wp_head(); ?>
 
-    <script type="text/javascript" src="//use.typekit.net/uxx3dqf.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
     <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
   </head>
