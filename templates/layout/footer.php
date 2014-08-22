9228ab17920d79cd18181aee5c66cd94f9dbf345
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-22 10:43:11 +0530

    typekit async

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,4 +1,11 @@
 <?php wp_footer(); ?>
 
-<script type="text/javascript" src="//use.typekit.net/uxx3dqf.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<script type="text/javascript">
+  (function(d) {
+    var config = {
+      kitId: 'uxx3dqf',
+      scriptTimeout: 15000
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
